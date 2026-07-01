6b4cb49bd9beda0ea98b208a448ef572c6e23a93
Ngô Văn Cảnh <nv.canh@outlook.com>
2026-07-01 15:43:08 +0700

    Change web title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Movie Invitation",
+  title: "Obsession",
   description: "An interactive date invitation but NO is not an option",
 };
 
